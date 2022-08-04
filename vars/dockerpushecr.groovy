bc14e356ba20a2d0ebb6527886b06df797e0397b
vin2008vin <39330479+vin2008vin@users.noreply.github.com>
2022-08-05 02:16:15 +0530

    Update dockerpushecr.groovy

--- vars/dockerpushecr.groovy
+++ vars/dockerpushecr.groovy
@@ -1,6 +1,6 @@
 def call(){
    sh '''
-        docker tag nginx:latest 647834768285.dkr.ecr.ap-south-1.amazonaws.com/nginx:latest
-        docker push 647834768285.dkr.ecr.ap-south-1.amazonaws.com/nginx:latest
+        docker tag nginx:0.1.0 647834768285.dkr.ecr.ap-south-1.amazonaws.com/nginx:0.1.0
+        docker push 647834768285.dkr.ecr.ap-south-1.amazonaws.com/nginx:0.1.0
     '''
 }
