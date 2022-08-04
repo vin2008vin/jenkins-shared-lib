3f34f5b07e9dab2ee202cec2a6348829be721138
vin2008vin <39330479+vin2008vin@users.noreply.github.com>
2022-08-05 01:54:06 +0530

    Update dockerpushecr.groovy

--- vars/dockerpushecr.groovy
+++ vars/dockerpushecr.groovy
@@ -1,6 +1,6 @@
 def call(){
    sh '''
-        docker tag sample:0.1.0 647834768285.dkr.ecr.ap-south-1.amazonaws.com/sample:0.1.0
+        docker tag nginx:alpine 647834768285.dkr.ecr.ap-south-1.amazonaws.com/nginx:alpine
         docker push 647834768285.dkr.ecr.ap-south-1.amazonaws.com/sample:0.1.0
     '''
 }
