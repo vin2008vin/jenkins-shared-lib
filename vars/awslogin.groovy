ed5fd1d4c1cfd35c445269106c5c527465d2bef9
vin2008vin <39330479+vin2008vin@users.noreply.github.com>
2022-08-05 00:20:36 +0530

    Update awslogin.groovy

--- vars/awslogin.groovy
+++ vars/awslogin.groovy
@@ -2,7 +2,7 @@ import com.aws.example.*
 
 def call() {
             sh ''' 
-			aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 647834768285.dkr.ecr.ap-south-1.amazonaws.com
-			'''
+		aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 647834768285.dkr.ecr.ap-south-1.amazonaws.com
+	    '''
         }
     
