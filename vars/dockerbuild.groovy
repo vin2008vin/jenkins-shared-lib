6ad43ec77ef240d5aeb20256e459d0693d18c18e
vin2008vin <39330479+vin2008vin@users.noreply.github.com>
2022-08-05 00:47:37 +0530

    Update dockerbuild.groovy

--- vars/dockerbuild.groovy
+++ vars/dockerbuild.groovy
@@ -1,5 +1,6 @@
 def call() {
+    dir("${env.JENKINS_HOME}/jobs/${env.JOB_NAME}/builds/${env.BUILD_NUMBER}/libs/jenkins-shared-lib/resources")
     sh '''
-        docker build -t ${WORKSPACE}/resource/Dockerfile sample .
+        docker build -t sample .
     '''
 }
